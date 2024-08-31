a90d217593f21106a5c8ccab10c4e57cb14e169d
Vyshnav <vyshnavk78@gmail.com>
2024-09-01 00:37:47 +0530

    script changes for a tag

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
     const userList = document.getElementById("user-list").getElementsByTagName("li");
-    
     const lastUser = userList[0].innerText;
     document.getElementById("last-user").innerText += " "+lastUser;
-})
+
+    const links = document.querySelectorAll("#user-list a");
+    links.forEach(link => {
+        link.setAttribute("target", "_blank");
+        link.setAttribute("rel", "noopener noreferrer");
+    });
+
+});
